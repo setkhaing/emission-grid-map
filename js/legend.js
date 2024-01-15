efbe3a2c67a4cb451dec7b4210d6486fb2fcb548
setkhaing <setkhaing@gmail.com>
2024-01-15 23:23:05 +0630

    Modified legend

--- js/legend.js
+++ js/legend.js
@@ -3,19 +3,21 @@ function updateLegend(layerType) {
     if (layerType === "totalEmission") {
       legend.innerHTML = `
           <h4>Average Total Emission (kg) </h4>
-          <div><span style="background-color: #2b83ba; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> <0.054 kg</div>
-          <div><span style="background-color: #b2df8a; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.054 kg to 0.074 kg</div>
-          <div><span style="background-color: #fee9cd; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.074 kg to 0.103 kg</div>
-          <div><span style="background-color: #d75f61; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.103 kg </div>
+          <div><span style="background-color: #d75f61; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.137 kg </div>
+          <div><span style="background-color: #fee9cd; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.105 kg to 0.137 kg</div>
+          <div><span style="background-color: #b2df8a; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.080 kg to 0.105 kg</div>
+          <div><span style="background-color: #2b83ba; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> <0.080 kg</div>
+
       </div>
           `;
     } else if (layerType === "emissionRate") {
       legend.innerHTML = `
           <h4>Emission Rate (kg/km) </h4>
-          <div><span style="background-color: #2b83ba; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> <0.148 kg/km</div>
-          <div><span style="background-color: #b2df8a; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.148 kg/km to 0.158 kg/km</div>
-          <div><span style="background-color: #fee9cd; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.158 kg/km to 0.168 kg/km</div>
-          <div><span style="background-color: #d75f61; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.168 kg/km</div>
+          <div><span style="background-color: #d75f61; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.177 kg/km</div>
+          <div><span style="background-color: #fee9cd; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.165 kg/km to 0.177 kg/km</div>
+          <div><span style="background-color: #b2df8a; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> >0.152 kg/km to 0.165 kg/km</div>
+          <div><span style="background-color: #2b83ba; display: inline-block; width: 20px; height: 20px; margin-right: 5px;"></span> <0.152 kg/km</div>
+
       </div>
           `;
     } else {
